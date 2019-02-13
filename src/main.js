bec3efa9c3d93f4a89f4a9cda130e340bd374e6e
John Galindez <jfrankgalindez@sipay.es>
2019-02-13 17:39:12 +0100

    removing firebase

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import './plugins/element.js'
 import './plugins/font-awesome.js'
 import './plugins/paginate.js'
 import i18n from './plugins/i18n'
-import firebase from 'firebase'
 import VueMoment from 'vue-moment'
 
 Vue.use(VueMoment)
@@ -14,18 +13,6 @@ Vue.config.debug = process.env.NODE_ENV === 'development'
 Vue.config.silent = process.env.NODE_ENV === 'production'
 Vue.config.devtools = true
 Vue.config.productionTip = false
-
-// Initialize Firebase
-var config = {
-  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
-  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID
-}
-
-firebase.initializeApp(config)
 
 new Vue({
   router,
